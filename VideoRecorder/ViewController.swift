8a26a2db4eddacd9033118c64391903446e2825e
m01i0ng <alley.ma@qq.com>
2020-10-09 14:03:25 +0800

    Initial Commit

---
+++ VideoRecorder/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VideoRecorder
+//
+//  Created by 马良 on 2020/10/9.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
